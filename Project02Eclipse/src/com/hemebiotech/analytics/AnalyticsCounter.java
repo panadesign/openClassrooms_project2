141709742d6447403c0483464197400c5c0a53fe
Jeremy Charpentier <charpentier.jeremy@gmail.com>
2021-12-02 14:19:32 +0100

    sort and count symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,6 +1,8 @@
 package com.hemebiotech.analytics;
 
 import com.hemebiotech.analytics.read.ReadSymptomDataFromFile;
+import com.hemebiotech.analytics.sort.ISymptomSort;
+import com.hemebiotech.analytics.sort.SortSymptoms;
 
 import java.util.List;
 
@@ -9,5 +11,9 @@ public class AnalyticsCounter {
 	public static void main(String[] args) throws Exception {
 		ReadSymptomDataFromFile readSymptom = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
 		List<String> symptomsList = readSymptom.GetSymptoms();
+
+		ISymptomSort sortSymptoms = new SortSymptoms();
+		sortSymptoms.mapSymptoms(symptomsList);
+
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/sort/SortSymptoms.java
@@ -0,0 +1,24 @@
+package com.hemebiotech.analytics.sort;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class SortSymptoms implements ISymptomSort {
+
+	public SortSymptoms(){}
+
+	public Map<String, Integer> mapSymptoms (List<String> symptomsList) {
+		Map<String, Integer> map = new TreeMap<>();
+
+		for(int i=0; i<symptomsList.size(); i++) {
+			String currentSymptom = symptomsList.get(i);
+			int countSymptom = map.getOrDefault(currentSymptom, 0) + 1;
+			map.put(currentSymptom, countSymptom);
+		}
+
+		return map;
+
+	}
+
+}
